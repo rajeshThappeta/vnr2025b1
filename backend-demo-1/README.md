75c5e2e90793de52039a60fdd65165ec5ea9a700
rajesh <rajesh.t7982@gmail.com>
2025-10-15 20:05:53 +0530

    complate user api with local data

--- backend-demo-1/README.md
+++ backend-demo-1/README.md
@@ -3,3 +3,9 @@
 
 2. Create HTTP SERVER ( express module )  
         npm install express
+
+3. Defines routes(API)
+        app.METHOD( path , (req,res)=>{} )
+
+4. To run server in watch mode which restarts automatically when code changes
+        npm install -g nodemon
--- backend-demo-1/server.js
+++ backend-demo-1/server.js
@@ -3,7 +3,28 @@ import exp from "express";
 const app = exp();
 
 //add body parser middleware
-app.use(exp.json())
+app.use(exp.json());
+
+//middleware-1
+function middleware1(req, res, next) {
+  console.log("middleware-1");
+  //req verification business logic
+  //either send res back to client or forward req to next middleware
+  // res.json({message:"This res from middleware-1"})
+  next();
+}
+
+//middleware-2
+function middleware2(req, res, next) {
+  console.log("middleware-2");
+  next();
+}
+
+//use middleware
+//execute middleware for every req
+//  app.use(middleware1)
+app.use(middleware2);
+//execute middleware for a specific req
 
 //assign a port number to HTTP SERVER
 const port = 8080;
@@ -18,7 +39,7 @@ let usersList = [
 //CREATE USER API ( routes for create user,read user,update user and delete user)
 
 // route read users
-app.get("/users", (req, res) => {
+app.get("/users", middleware1, (req, res) => {
   //send res
   res.json({ message: "all users", payload: usersList });
 });
@@ -38,20 +59,49 @@ app.get("/users/:userId", (req, res) => {
 });
 
 // route create user
-    app.post('/user',(req,res)=>{
-            //get body from req
-            let newUser=req.body
-           //push newUser into users
-           usersList.push(newUser)
-           //res
-           res.json({meesage:"new user created"})
-    })
-
-
+app.post("/user", (req, res) => {
+  //get body from req
+  let newUser = req.body;
+  //push newUser into users
+  usersList.push(newUser);
+  //res
+  res.json({ meesage: "new user created" });
+});
 
 // route update user by id
+app.put("/user/:userId", (req, res) => {
+  //get modified user from client
+  let modifiedUser = req.body;
+  //replace userObj in usersList with modifiedUser
+  let userIndex = usersList.findIndex((userObj) => userObj.id === modifiedUser.id);
+  //if user not found
+  if (userIndex === -1) {
+    res.send({ message: "User not found to update" });
+  } else {
+    usersList.splice(userIndex, 1, modifiedUser);
+    res.send({ message: "User updated" });
+  }
+});
+
 // route delete user by id
+app.delete("/user/:userId", (req, res) => {
+  //get user id from URL path
+  let idOfUser = Number(req.params.userId);
+  //get index of userObj to delete
+  let userIndex = usersList.findIndex((userObj) => userObj.id === idOfUser);
+  //if user not found
+  if (userIndex === -1) {
+    res.json({ message: "User not found to delete" });
+  } else {
+    usersList.splice(userIndex, 1);
+    res.json({ message: "User deleted" });
+  }
+});
 
 
-
-
+//Product API
+    //create product
+    //read all prods
+    //read a prod by id
+    //update a prod by id
+    //delete a prod by id
